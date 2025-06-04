699820779abb7a2a943370e4eff65a9791459635
ASHOK2706 <ashokkumar27b@gmail.com>
2025-06-04 21:59:06 +0530

    sql practice on 04-06-2025

--- _44_FindTheStartAndEndNumberOfContinuousRanges.sql
+++ _44_FindTheStartAndEndNumberOfContinuousRanges.sql
@@ -21,3 +21,15 @@ values
 
 -- Solution:
 
+with cte as (
+    select 
+        log_id,
+        log_id - row_number() over(order by log_id) as diff
+    from logs
+)
+select 
+    min(log_id) as start_id,
+    max(log_id) as end_id
+from cte
+group by diff
+order by start_id;
